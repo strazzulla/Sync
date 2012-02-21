de4887c615da3bcda863f0767765b316cb8017c3
johnss518 <stacy.m.johnson@gmail.com>
2012-02-20 21:32:58 -0800

    Removed the register button in footer

--- mvp3/mvp/db/schema.rb
+++ mvp3/mvp/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120212100456) do
+ActiveRecord::Schema.define(:version => 20120212101308) do
+
+  create_table "user_sessions", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
