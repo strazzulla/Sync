5ac17c94404faa928f4e82c4a731407d3c6b2656
Daniel Strazzulla <strazzulla@gmail.com>
2012-03-02 02:28:31 -0800

    added iOS app code to git

--- mvp3/mvp/app/controllers/users_controller.rb
+++ mvp3/mvp/app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
+    @users = User.all
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @user }
--- mvp3/mvp/app/models/user.rb
+++ mvp3/mvp/app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  validates_presence_of :email, :partner_email
 end
